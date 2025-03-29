c1ea1e5fb612ca1c58bb9936880677471d3d27c0
zzsqjdhqgb <zzsqjdhqgb@outlook.com>
2025-03-29 09:13:44 +0800

    switch header to debug

--- CnblogDownloader.user.js
+++ CnblogDownloader.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
-// @name         Cnblog Downloader
+// @name         Cnblog Downloader (DEBUG)
 // @namespace    https://github.com/zzsqjdhqgb/
-// @version      0.1.1
+// @version      0.1.1.1
 // @description  下载博客园的文章为 Markdown 文件，目前仅为功能不完整的临时版本
 // @author       zzsqjdhqgb
 // @match        https://www.cnblogs.com/*
 // @grant        none
-// @updateURL    https://zzsqjdhqgb.github.io/Cnblogs-Downloader/CnblogDownloader.js
-// @downloadURL    https://zzsqjdhqgb.github.io/Cnblogs-Downloader/CnblogDownloader.js
+// @updateURL    https://zzsqjdhqgb.github.io/Cnblogs-Downloader/DEBUG/CnblogDownloader.user.js
+// @downloadURL    https://zzsqjdhqgb.github.io/Cnblogs-Downloader/DEBUG/CnblogDownloader.user.js
 // ==/UserScript==
 
 (async () => {
